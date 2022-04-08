5166befc485442b301d9c0185f735484b9d1b36f
Shaw Lu <shawlu95@gmail.com>
2022-04-08 09:23:02 -0700

    Styled Navbar

---
+++ src/components/StyledNavBar.js
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <NavLink
+        to='/'
+        style={({ isActive }) => {
+          return { color: isActive ? 'link active' : 'link' };
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/about'
+        style={({ isActive }) => {
+          return { color: isActive ? 'link active' : 'link' };
+        }}>
+        About
+      </NavLink>
+      <NavLink
+        to='/products'
+        style={({ isActive }) => {
+          return { color: isActive ? 'link active' : 'link' };
+        }}
+      >
+        Products
+      </NavLink>
+    </nav>
+  );
+};
+export default Navbar;
--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,10 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import StyledNavBar from '../components/StyledNavBar';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <StyledNavBar />
       <Outlet />
     </>
   );
