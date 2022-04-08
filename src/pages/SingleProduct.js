7bd7e03e50f73fbfec70921597761db9ccbaab62
Shaw Lu <shawlu95@gmail.com>
2022-04-08 09:27:58 -0700

    URL Param

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,12 @@
+import { Link, useParams } from 'react-router-dom';
+import products from '../data';
 const SingleProduct = () => {
+  const { productId } = useParams();
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>{productId}</h2>
+      <Link to='/products'>back to products</Link>
     </section>
   );
 };
